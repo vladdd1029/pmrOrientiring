f7991d4d8be29d244976a15d2c01bfcd7dd79185
jucmi <vorobevvlad1029@mail.ru>
2025-05-14 02:35:36 +1000

    Изменен способ рендеринга приложения с использованием createRoot вместо ReactDOM.render для улучшения совместимости с новыми версиями React.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import { UserProvider } from './context/UserContext';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
-  <UserProvider>
-    <Router>
-      <App />
-    </Router>
-  </UserProvider>,
-  document.getElementById('root')
+createRoot(document.getElementById('root')).render(
+    <UserProvider>
+      <Router>
+        <App />
+      </Router>
+    </UserProvider>
 );
