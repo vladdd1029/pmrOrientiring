ca3cc77175cc7f592c09219841ee1e0207dc753a
jucmi <vorobevvlad1029@mail.ru>
2025-05-28 00:02:20 +1000

    chore: задать homepage и basename для GitHub Pages

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
- // 1) Инициализируем QueryClient
- const queryClient = new QueryClient();
+// 1) Инициализируем QueryClient
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <UserProvider>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
