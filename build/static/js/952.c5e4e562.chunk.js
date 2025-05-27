fd3308b06d48b8c4c2b23eb221230d1f4a081b62
jucmi <vorobevvlad1029@mail.ru>
2025-05-27 23:55:13 +1000

    Add map background image and update homepage URL in package.json

---
+++ build/static/js/952.c5e4e562.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknew_website=self.webpackChunknew_website||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.c5e4e562.chunk.js.map
